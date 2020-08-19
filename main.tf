4bf7c0685c9a19f1f0590ddc7ce71d64337e6889
Liam F. O'Neill <liamfoneill@outlook.com>
2020-08-19 10:36:51 +0100

    fixes provider block

--- main.tf
+++ main.tf
@@ -1,10 +1,3 @@
-provider "azuredevops" {
-  version = ">= 0.0.1"
-  source = "terraform-providers/azuredevops"
-  org_service_url = "https://dev.azure.com/Intelion-UK"
-  personal_access_token = "AZDO_PERSONAL_ACCESS_TOKEN"
-}
-
 resource "azuredevops_project" "test" {
   project_name       = "Terraform"
   description        = "Project Description"
@@ -13,4 +6,16 @@ resource "azuredevops_project" "test" {
 terraform {
   backend "azurerm" {
   }
+  required_providers {
+    azuredevops = {
+      # The "hashicorp" namespace is the new home for the HashiCorp-maintained
+      # provider plugins.
+      #
+      # source is not required for the hashicorp/* namespace as a measure of
+      # backward compatibility for commonly-used providers, but recommended for
+      # explicitness.
+      source  = "terraform-providers/azuredevops"
+      org_service_url = "https://dev.azure.com/Intelion-UK"
+      personal_access_token = "AZDO_PERSONAL_ACCESS_TOKEN"
+    }
 }
