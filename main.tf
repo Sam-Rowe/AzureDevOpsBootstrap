ce33ef0f7aeae03df21242a6f06cabd8b14ae6a0
Liam F. O'Neill <liamfoneill@outlook.com>
2020-08-19 10:32:04 +0100

    add empty backend block

--- main.tf
+++ main.tf
@@ -1,8 +1,15 @@
 provider "azuredevops" {
   version = ">= 0.0.1"
+  org_service_url = "https://dev.azure.com/Intelion-UK"
+  personal_access_token = "AZDO_PERSONAL_ACCESS_TOKEN"
 }
 
 resource "azuredevops_project" "test" {
   project_name       = "Terraform"
   description        = "Project Description"
+}
+
+terraform {
+  backend "azurerm" {
+  }
 }
