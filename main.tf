1e50deb95a0c9ac6a644aba61344d73c88f14cec
Liam F. O'Neill <liamfoneill@outlook.com>
2020-08-20 13:32:32 +0100

    remove build def

--- main.tf
+++ main.tf
@@ -59,35 +59,3 @@ resource "azuredevops_serviceendpoint_azurerm" "endpointazure" {
   azurerm_subscription_id   = var.subscription_id
   azurerm_subscription_name = var.subscription_name
 }
-
-resource "azuredevops_build_definition" "build" {
-  project_id = azuredevops_project.test.id
-  name       = "Sample Build Definition"
-  path       = "\\ExampleFolder"
-
-  ci_trigger {
-    use_yaml = true
-  }
-
-  repository {
-    repo_type   = "TfsGit"
-    repo_id     = azuredevops_git_repository.repository.id
-    branch_name = azuredevops_git_repository.repository.default_branch
-    yml_path    = "azure-pipelines.yml"
-  }
-
-  variable_groups = [
-    azuredevops_variable_group.vars.id
-  ]
-
-  variable {
-    name  = "PipelineVariable"
-    value = "Go Microsoft!"
-  }
-
-  variable {
-    name      = "PipelineSecret"
-    secret_value     = "ZGV2cw"
-    is_secret = true
-  }
-}
